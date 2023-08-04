7462333d00229773d9d9b2c5013136e7a318192f
Rameez Raja <ovaidmushtaq.ovi@gmail.com>
2023-08-04 11:38:48 +0530

    niit basics

---
+++ java.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		String s = sc.nextLine();
+		
+		for(int i=0; i<str.length(); i++){
+		   System.out.println(str);
+		}
+        
+	}
+}
+
